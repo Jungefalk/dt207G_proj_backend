8ac81103d5cd7eef43d4465e01f31f9d990566d3
Caroline <c.jungefalk@gmail.com>
2025-06-03 21:22:45 +0200

    Skapa schemas

--- server.js
+++ server.js
@@ -9,7 +9,7 @@
 //Importera paket
 const express = require("express");
 const cors = require("cors");
-const mongoose  = require("mongoose");
+const mongoose = require("mongoose");
 
 //initiera express
 const app = express();
@@ -18,3 +18,72 @@ const port = 3000;
 //Middleware
 app.use(cors());
 app.use(express.json());
+
+/**
+ * Schemas
+ */
+
+//Gelato schema
+
+const gelatoSchema = new mongoose.gelatoSchema({
+    name: {
+        type: String,
+        required: [true, "Skicka med namn på produkt"],
+
+    },
+    ingredients: {
+        type: String,
+        required: [true, "Skicka med ingredientslista"]
+    },
+    info: {
+        type: String,
+        required: [false]
+    },
+    price: {
+        type: String,
+        required: [true, "Skicka med pris på produkt"]
+    }
+});
+
+//Topping schema
+
+const toppingSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Skicka med namn på produkt"]
+    },
+    price: {
+        type: String,
+        required: [true, "Skicka med pris på produkt"]
+    }
+});
+
+// Schema för dryck
+
+const drinkSchema = new mongoose.Schema({
+     name: {
+        type: String,
+        required: [true, "Skicka med namn på produkt"]
+    },
+    price: {
+        type: String,
+        required: [true, "Skicka med pris på produkt"]
+    }
+});
+
+// Schema för kommentar
+
+const commentSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: [true, "Skicka med namn"]
+    },
+    rating:{
+    type: String,
+    required: [true, "Skicka med ett betyg"]
+    },
+    comment:{
+        type: String,
+        required: [false]
+    }
+});
