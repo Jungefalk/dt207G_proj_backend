a5d19b83fa01ec0793f6eb628e1f16c1f12c47c9
Caroline <c.jungefalk@gmail.com>
2025-06-04 22:44:35 +0200

    Lägg till hash av lösenord med bcrypt

--- Models/adminModel.js
+++ Models/adminModel.js
@@ -3,6 +3,7 @@
  */
 
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 //Schema
 const adminSchema = new mongoose.Schema({
@@ -15,5 +16,20 @@ const adminSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
+    }
+});
+
+//Hasha lösenord innan användare sparas
+adminSchema.pre("save", async function (next) {
+
+    try {
+        if (this.isNew || this.isModified("password")) {
+            const hashedPassword = await bcrypt.hash(this.password, 10);
+            this.password = hashedPassword;
+        }
+        next();
+
+    } catch (error) {
+        next(error)
     }
 });
