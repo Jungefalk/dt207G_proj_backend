0cfd0ec578e7c34c81c3f2bb6c678b053ed170f5
Caroline <c.jungefalk@gmail.com>
2025-06-04 21:46:06 +0200

    Installera bcrypt och skapa schema för adminanvändare

--- Models/adminModel.js
+++ Models/adminModel.js
@@ -1,3 +1,19 @@
 /**
  * Schema och Model för Admin
  */
+
+const mongoose = require("mongoose");
+
+//Schema
+const adminSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -11,14 +11,14 @@ router.post("/register", async (req, res) => {
         const { username, password } = req.body;
 
         //validera input
-        if(!username || username.length < 5 || !password || password.length < 5){
-            return res.status(400).json({error: "Skicka med användarnamn och lösenord som är längre än 5 tecken"})
+        if (!username || username.length < 5 || !password || password.length < 5) {
+            return res.status(400).json({ error: "Skicka med användarnamn och lösenord som är längre än 5 tecken" })
         };
 
-        res.status(201).json({message: "Användare tillagd"})
+        res.status(201).json({ message: "Användare tillagd" })
 
     } catch (error) {
-        res.status(500).json(error);
+        res.status(500).json({ error: "Det blev ett serverfel" });
     }
 });
 
