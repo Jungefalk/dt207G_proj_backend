b9817dbedf680b149829acd6242b897dce29128b
Caroline <c.jungefalk@gmail.com>
2025-06-05 10:44:43 +0200

    Lägg till post-route med kontroller för att logga in användare

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -17,10 +17,42 @@ router.post("/register", async (req, res) => {
         };
 
         //Spara användare vid korrekt input
-        const admin = new Admin({ username, password})
+        const admin = new Admin({ username, password })
         await admin.save();
 
         res.status(201).json({ message: "Användare tillagd" })
+
+    } catch (error) {
+        res.status(500).json({ error: "Det blev ett serverfel" });
+    }
+});
+
+//Logga in användare
+
+router.post("/login", async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        //validera input
+        if (!username || !password) {
+            return res.status(400).json({ error: "Skicka med användarnamn/lösenord" });
+        }
+
+        //Kontrollera inloggningsuppgifter
+        //Användarnamn
+        const admin = await Admin.findOne({ username });
+        if (!admin) {
+            return res.status(401).json({ error: "Felaktigt användarnamn/lösenord" });
+        };
+
+        //Lösenord
+
+        const correctPassword = await admin.checkPassword(password);
+        if (!correctPassword) {
+            return res.status(401).json({ error: "Felaktigt användarnamn/lösenord" });
+        } else {
+            res.status(200).json({ message: "Användare inloggad" });
+        }
 
     } catch (error) {
         res.status(500).json({ error: "Det blev ett serverfel" });
